20360fd90a49d411446072b3a7cb72ca06fd9429
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2019-02-25 18:01:15 +0300

    Update init.php

--- app/init.php
+++ app/init.php
@@ -3,5 +3,5 @@ session_start();
 $_SESSION['user_id'] = (int) 2 ; // hardcoded for make it easy 
 
 // OOP mysqli 
-$db = new mysqli('localhost', 'root', '' ,'likeButton'); 
+$db = new mysqli('YOURHOST', 'DB_USERNAME', 'DB_PASSWORD' ,'DB_NAME'); 
 
